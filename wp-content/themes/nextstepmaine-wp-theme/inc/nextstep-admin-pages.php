3c09d2296d69ce92884a98579e1c666715ad477d
James <jamesmehorter@gmail.com>
2012-06-14 14:17:12 -0400

    Installed the ONET database, and modified the job importer to pull some data from the db.

--- wp-content/themes/nextstepmaine-wp-theme/inc/nextstep-admin-pages.php
+++ wp-content/themes/nextstepmaine-wp-theme/inc/nextstep-admin-pages.php
@@ -34,40 +34,52 @@
 				while (($job_row = fgetcsv($uploaded_csv, 0, ',', '"')) !== false) :
 					
 					//DEBUGGING
-					//echo "<pre>" . print_r($data_row, true) . "</pre>";
+					//echo "<pre>" . print_r($job_row, true) . "</pre>";
 					/*
 					Array
 					(
-						[0] => High School   // nsm_job_category
-						[1] => 811			 // _employment_2008
-						[2] => 803			 // _employment_2018
-						[3] => -1.0%		 // _growth_rate
-						[4] => 21			 // _annual_openings
-						[5] => $12.26		 // _entry_wage
-						[6] => $16.94		 // _median_wage
-						[7] => http://online.onetcenter.org/link/summary/49-3021.00		// _onet_link
-						[8] => Automotive Body and Related Repairers					// post title
+						[0] => 49-3021		//_soc_code
+						[1] => Automotive Body and Related Repairers	// post title
+						[2] => 811 			// _employment_2008
+						[3] => 803			// _employment_2018
+						[4] => -1.0%		// _growth_rate
+						[5] => 21			// _annual_openings
+						[6] => $12.26		// _entry_wage
+						[7] => $16.94		// _median_wage
+						[8] => http://online.onetcenter.org/link/summary/49-3021.00		// _onet_link
+						[9] => Automotive Body and Related Repairers	// post title - DUPLICATE - uneeded
+ 						[10] => High School // nsm_job_category
 					)
+					
+					
 					*/
+					
+					//Connect to our O*NET database
+					$onet = new wpdb('root', '1309piCa', 'onet', 'localhost');
+					
+					//Select some information about the current occupation
+					$occupation = $onet->get_results("SELECT description FROM occupation_data WHERE onetsoc_code LIKE '%{$job_row[0]}%'");
 					
 					//Create a custom post for each row
 					$job_post_id = wp_insert_post(array(
 						'post_type'	  => 'nsm_job',
 						'post_status' => 'publish',
-						'post_title'  => $job_row[8]
+						'post_title'  => $job_row[1],
+						'post_content' => $occupation[0]->description
 					));	
 					
 					//Add the category term to the post
-					wp_set_object_terms($job_post_id, $job_row[0], 'nsm_job_category');
+					wp_set_object_terms($job_post_id, $job_row[10], 'nsm_job_category');
 						
 					//Add the remaining data as post meta
-					update_post_meta($job_post_id, '_employment_2008', $job_row[1]);
-					update_post_meta($job_post_id, '_employment_2018', $job_row[2]);
-					update_post_meta($job_post_id, '_growth_rate', $job_row[3]);
-					update_post_meta($job_post_id, '_annual_openings', $job_row[4]);
-					update_post_meta($job_post_id, '_entry_wage', $job_row[5]);
-					update_post_meta($job_post_id, '_median_wage', $job_row[6]);
-					update_post_meta($job_post_id, '_onet_link', $job_row[7]);
+					update_post_meta($job_post_id, '_soc_code', $job_row[0]);
+					update_post_meta($job_post_id, '_employment_2008', $job_row[2]);
+					update_post_meta($job_post_id, '_employment_2018', $job_row[3]);
+					update_post_meta($job_post_id, '_growth_rate', $job_row[4]);
+					update_post_meta($job_post_id, '_annual_openings', $job_row[5]);
+					update_post_meta($job_post_id, '_entry_wage', $job_row[6]);
+					update_post_meta($job_post_id, '_median_wage', $job_row[7]);
+					update_post_meta($job_post_id, '_onet_link', $job_row[8]);
 					
 				endwhile;
 				
--- wp-content/themes/nextstepmaine-wp-theme/single-nsm_job.php
+++ wp-content/themes/nextstepmaine-wp-theme/single-nsm_job.php
@@ -5,16 +5,18 @@
             	<?php while (have_posts()) : the_post() ?>
                 	<h2><?php the_title() ?></h2>
                     <br />
-					Employment 2008: <?php echo get_post_meta($post->ID, '_employment_2008', true) ?><br />
-					Employment 2018: <?php echo get_post_meta($post->ID, '_employment_2018', true) ?><br />
-                    Growth Rate: <?php echo get_post_meta($post->ID, '_growth_rate', true) ?><br />
+                    <?php the_content() ?>
+                    <br /><br />
+					Number of Jobs in 2008: <?php echo get_post_meta($post->ID, '_employment_2008', true) ?><br />
+					Number of Jobs in 2018: <?php echo get_post_meta($post->ID, '_employment_2018', true) ?><br />
+                    Yearly Job Growth Rate: <?php echo get_post_meta($post->ID, '_growth_rate', true) ?><br />
                     Annual Openings: <?php echo get_post_meta($post->ID, '_annual_openings', true) ?><br />
                     Entry Wage: <?php echo get_post_meta($post->ID, '_entry_wage', true) ?><br />
                     Median Wage: <?php echo get_post_meta($post->ID, '_median_wage', true) ?><br />
                     <br />
                     <a href="<?php echo get_post_meta($post->ID, '_onet_link', true) ?>" title="<?php echo $post->post_title ?>" target="_blank">More Information</a> <br />
 					
-                	<?php the_content() ?>
+                	
                 <?php endwhile ?>
             <?php endif ?>
         </div>
