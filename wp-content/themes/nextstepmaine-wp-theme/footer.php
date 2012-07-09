09f98bfcc8635dcc93b4cfbaa69dd8a03df28275
James <jamesmehorter@gmail.com>
2012-07-09 16:59:53 -0400

    Minified jQuery Cycle

--- wp-content/themes/nextstepmaine-wp-theme/footer.php
+++ wp-content/themes/nextstepmaine-wp-theme/footer.php
@@ -43,7 +43,7 @@
             </footer>
         </section><!-- .website-wrapper -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    	<script src="<?php bloginfo('template_directory') ?>/scripts/jquery.cycle.all.js"></script>
+    	<script src="<?php bloginfo('template_directory') ?>/scripts/jquery.cycle.all.min.js"></script>
         <script src="<?php bloginfo('template_directory') ?>/scripts/jquery.next-step-maine.js"></script>
     </body>
 </html>
