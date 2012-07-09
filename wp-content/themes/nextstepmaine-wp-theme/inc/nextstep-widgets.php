014c06092abf29c80c0d94954dc895da3fd3f65e
James <jamesmehorter@gmail.com>
2012-07-09 09:53:33 -0400

    Implemented a condition for the instutition dropdown widget to add a selected attr on single program pages.

--- wp-content/themes/nextstepmaine-wp-theme/inc/nextstep-widgets.php
+++ wp-content/themes/nextstepmaine-wp-theme/inc/nextstep-widgets.php
@@ -108,34 +108,59 @@
             <section class="widget" id="institutions">
                 <h3>Institutions</h3>
                 <p>(Choose from one of the<br />options in the drop down menu<br /><span class="blue-text"><strong>to search for program types<br />and schools)</strong></span></p>
-                
                 <select>
                 	<option value="">Institutions</option>
                 <?php
+                	//Query all the institutions to populate our dropdown menu
 					$institutions = new WP_Query(array(
 						'post_type' => 'nsm_institution',
 						'posts_per_page' => -1
-					));
-					
+					));					
+					//cache the current page(post) for a condition (we're about to overwrite it)
 					$current_page = $post;
+					$current_insitution = "";
 
 					while ($institutions->have_posts()) : $institutions->the_post(); 
-						$institution = p2p_type( 'Program Institution' )->get_connected( $post );
-						
-						/*
-						while ( $institution->have_posts() ) : $institution->the_post(); 
-							$institution_slug = $post->post_name ;
-						endwhile ; wp_reset_query() ;
-						*/
-						
-						if ($current_page->post_name == $post->post_name || $institution_slug == $post->post_name) : 
-							$selected = "selected='selected'";
-						else :
-							$selected = "";
+						/* VIEWING SINGLE PROGRAM */
+						if ($current_page->post_type == "nsm_program") : 
+							//Cache the current $post as we're about to overwrite it
+							$insitution_post = $post;
+
+							// Find the current program's institution	
+							$institution = p2p_type( 'Program Institution' )->get_connected( $current_page );
+							while ( $institution->have_posts() ) : $institution->the_post(); 
+								//cache the institution slug for a conditional later on down the page
+								$program_institution_slug = $post->post_name;
+
+								if ($post->post_name == $insitution_post->post_name) : 
+									$current_insitution = "selected='selected'";
+								else :
+									$current_insitution = "";
+								endif;
+
+							endwhile; wp_reset_postdata(); 
+
+							//Reinstate the previous $post
+							$post = $insitution_post;
+						endif;
+
+						/* VIEWING SINGLE INSTITUTION */
+						//If the user is currently viewing an institution mark the matching institution / parent institution as selected 
+						if ($current_page->post_type == 'nsm_institution') :
+							if ($current_page->post_name == $post->post_name) : 
+								$current_insitution = "selected='selected'";
+							else :
+								$current_insitution = "";
+							endif;
 						endif;
 					?>
-						<option value="<?php the_permalink() ?>" <?php echo $selected ?>><?php the_title() ?></option>
-					<?php endwhile ?>
+						<option value="<?php the_permalink() ?>" <?php echo $current_insitution ?>><?php the_title() ?></option>
+					<?php 
+						endwhile; 
+						wp_reset_postdata(); 
+						//Reinstate the cached page(post) 
+						$post = $current_page ; 
+					?>
                  </select>
             </section>
             <?php
